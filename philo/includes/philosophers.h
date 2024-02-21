a474038d347a5b234f10c62c7087fe3ab72f1a7f
Alec Croue <alec.croue@gmail.com>
2024-02-21 16:01:00 +0100

    basic push

--- philo/includes/philosophers.h
+++ philo/includes/philosophers.h
@@ -6,7 +6,7 @@
 /*   By: acroue <acroue@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/20 17:41:17 by acroue            #+#    #+#             */
-/*   Updated: 2024/02/21 14:40:58 by acroue           ###   ########.fr       */
+/*   Updated: 2024/02/21 15:30:35 by acroue           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,11 @@
 # define PHILOSOPHERS_H
 
 # include <unistd.h>
+# include <string.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <sys/time.h>
+# include <pthread.h>
 
 #define EBAD_ARG "Error, wrong arguments : \
 ./philosophers number_of_philosopher\
@@ -21,3 +26,13 @@ s time_to_die time_to_eat time_to_sleep \
 [number_of_times_each_philosopher_must_eat]\n"
 
 #endif
+
+/*
+pthread_create,
+pthread_detach,
+pthread_join,
+pthread_mutex_init,
+pthread_mutex_destroy,
+pthread_mutex_lock,
+pthread_mutex_unlock
+ */
